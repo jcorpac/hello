9938a9eed403913f6d3984e9666d24c281fff8a1
Jeff Corpac <jcorpac@gmail.com>
2024-08-18 01:13:18 +0000

    Expanded Threadpool to create a finite pool of workers

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,59 @@
-pub struct ThreadPool;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread
+};
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
-    pub fn new(_size: usize) -> ThreadPool {
-        ThreadPool
+    /// Create a new ThreadPool.
+    /// 
+    /// The size is the number of threads in the pool.
+    /// 
+    /// # Panics
+    /// 
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+
+        let reveiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&reveiver)));
+        }
+
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
         where
             F: FnOnce() + Send + 'static,
         {
-            std::thread::spawn(f);
+            let job = Box::new(f);
+            self.sender.send(job).unwrap();
         }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(|| {
+            receiver;
+        });
+
+        Worker { id, thread }
+    }
 }
